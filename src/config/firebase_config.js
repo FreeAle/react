2a6a2e79a50ac878108bedabb46f8bb17d47d941
Ale <freeale12@gmail.com>
2021-03-02 17:06:52 +0100

    First commit

---
+++ src/config/firebase_config.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    "databaseURL": "https://sasreact-2d59a-default-rtdb.firebaseio.com/",
+    "apiKey": "AIzaSyBt804NGgT-lVwyWiOp26fJqHyyx6uajN0",
+    "authDomain": "sasreact-2d59a.firebaseapp.com",
+    "projectId": "sasreact-2d59a",
+    "storageBucket": "sasreact-2d59a.appspot.com",
+    "messagingSenderId": "310691467993",
+    "appId": "1:310691467993:web:45360b4641406d079764c8"
+}; 
+exports.config = firebaseConfig;
